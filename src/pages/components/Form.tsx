d8e7f14e24490d48b460addc10dd159708d009ae
Kazushige Shirahama <the.stone.monky.6317@gmail.com>
2023-05-01 16:17:56 +0900

    ✨ FromでAPIを叩いた

--- src/pages/components/Form.tsx
+++ src/pages/components/Form.tsx
@@ -1,11 +1,29 @@
-import React from "react";
+import { log } from "console";
+import React, { useState } from "react";
 
 export const Form = () => {
+  const [city, setCity] = useState<string>("");
+  const getWeather = (e: any) => {
+    e.preventDefault();
+    fetch(
+      "https://api.weatherapi.com/v1/current.json?key=0da1ce55290a4a60ba523948230105&q=London&aqi=no"
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
   return (
     <div>
       <form action="">
-        <input type="text" name="city" placeholder="都市名" />
-        <button type="submit">Get Weather</button>
+        <input
+          type="text"
+          name="city"
+          placeholder="都市名"
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button type="submit" onClick={getWeather}>
+          Get Weather
+        </button>
       </form>
     </div>
   );
