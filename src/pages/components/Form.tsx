a514d543d5b575c56b0263d4ecb47478ba7887aa
Kazushige Shirahama <the.stone.monky.6317@gmail.com>
2023-05-01 10:56:02 +0900

    ⚡️ formコンポーネントを作成

---
+++ src/pages/components/Form.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Form = () => {
+  return (
+    <div>
+      <form action="">
+        <input type="text" name="city" placeholder="都市名" />
+        <button type="submit">Get Weather</button>
+      </form>
+    </div>
+  );
+};
